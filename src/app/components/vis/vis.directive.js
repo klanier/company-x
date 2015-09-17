e06aab5e11ddadda0920bc7a19e826790b26fdbe
Katie Lanier <katie@140proof.com>
2015-09-17 16:36:07 -0700

    correct map scaling and adding cities

--- src/app/components/vis/vis.directive.js
+++ src/app/components/vis/vis.directive.js
@@ -1,3 +1,4 @@
+/* global _:false */
 class VisDirective {
   constructor () {
     'ngInject';
@@ -16,27 +17,22 @@ class VisDirective {
 
     function linkFunc(scope, el, attr, vm) {
       // Decrease width and height to allow for margin around svg
-      let width = angular.element('#vis')[0].clientWidth - 150;
-      let height = width / 2;
+      let width = angular.element('#vis')[0].clientWidth - 175,
+          height = (width / 2);
 
       let svg = d3.select('#vis').append('svg')
         .attr('width', width)
         .attr('height', height);
 
       let projection = d3.geo.equirectangular()
-        .scale(153)
-        .translate([(width / 2), ((height / 2))])
+        .scale(width * 0.159)
+        .translate([(width / 2), (height / 2)])
         .precision(0.1);
 
       let path = d3.geo.path()
         .projection(projection);
 
       let graticule = d3.geo.graticule();
-
-      svg.append('path')
-        .datum(graticule)
-        .attr('class', 'graticule')
-        .attr('d', path);
 
       d3.json('/assets/data/world-50m.json', function(error, world) {
         if (!error) {
@@ -45,6 +41,24 @@ class VisDirective {
             .attr('class', 'land')
             .attr('fill', '#eeeeee')
             .attr('d', path);
+
+          d3.json('/assets/data/major-cities.json', function(error, cities) {
+            if (!error) {
+              svg.selectAll('circle')
+                  .data(_.toArray(cities))
+                .enter().append('circle')
+                  .attr('transform', function(d) {
+                    let p = projection([d.lon, d.lat]);
+                    return 'translate(' + p[0] + ',' + p[1] + ')';
+                  })
+                  .attr('fill', 'rgba(15, 147, 67, 0.4)')
+                  .attr('r', function(d) {
+                    return Math.random() * 10;
+                  })
+                  .attr('cx', 2)
+                  .attr('cy', 2);
+            }
+          });
         }
       });
 
